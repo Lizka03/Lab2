67a39adaa60d27114812158716090039448c4bf7
Lizka03 <kobzeva0303@bk.ru>
2024-12-17 20:56:24 +0300

    v0

---
+++ Lr2_Kobzeva/Lr2_Kobzeva/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lr2_Kobzeva.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
